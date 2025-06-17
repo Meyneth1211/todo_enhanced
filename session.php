fa4b882bd16482a1020359736a11bfd52dda9c03
Meyneth1211 <2301389@s.asojuku.ac.jp>
2025-06-17 16:15:47 +0900

    仮完成
    DB接続、セッション管理、担当機能を仮完成

---
+++ session.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+    
+function displayname(){
+    if (!empty($_SESSION['userid'])) {
+        echo '<p>'.htmlspecialchars($_SESSION['username']).' さん <a href="logout.php">ログアウト</a></p>';
+    } else {
+        header('Location: login.php');
+        session_destroy();
+        exit();
+    }
+}
