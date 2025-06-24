eb0e00c4c43a39226dc6dd287a083ce55fedfa5b
Meyneth1211 <2301389@s.asojuku.ac.jp>
2025-06-24 09:58:35 +0900

    Comment out redirect to login in displayname()

--- session.php
+++ session.php
@@ -4,7 +4,7 @@ function displayname(){
     if (!empty($_SESSION['userid'])) {
         echo '<p>'.htmlspecialchars($_SESSION['username']).' さん <a href="logout.php">ログアウト</a></p>';
     } else {
-        header('Location: login.php');
+        //header('Location: login.php');
         session_unset();
         session_destroy();
         exit();
