cb121e03195e80c69fddff74c3e76a80fae2aafa
yano-haruto <2301318@s.asojuku.ac.jp>
2025-06-24 09:57:01 +0900

    login.phpコメントアウト
    login.phpコメントアウト

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+    <form action="" method="post">
+        <div class="box13">
+            <h1>ログイン</h1>
+            <div class="font">
+                ユーザー名: <input type="text" name="name"><br>
+                パスワード: <input type="password" name="password"><br>
+            </div>
+            <div class="btn-border-gradient-wrap btn-border-gradient-wrap--gold">
+                <a href="" class="btn btn-border-gradient"><span class="btn-text-gradient--gold">ログイン</span></a>
+            </div><br><br><br>
+            <div class="btn-border-gradient-wrap btn-border-gradient-wrap--gold">
+                <a href="" class="btn btn-border-gradient"><span class="btn-text-gradient--gold">新規登録</span></a>
+            </div>
+
+        </div>
+    </form>
+</body>
+
+</html>
