8de7ab34c9937f1e53bc8c1b28d7d47b4e47f2e3
Meyneth1211 <2301389@s.asojuku.ac.jp>
2025-06-30 10:01:54 +0900

    名前変更

--- task_edit.php
+++ task_edit.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="taskedit.css">
+    <link rel="stylesheet" href="task_edit.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
